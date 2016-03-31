cbf90b053bb0b7e6b89fa9698c93cfee26b94c9f
Ekin Akyürek <akyurekekin@gmail.com>
2016-03-31 23:28:05 +0300

    profile photo resizing

--- Molocate/editProfile.swift
+++ Molocate/editProfile.swift
@@ -294,10 +294,11 @@ class editProfile: UIViewController , UIImagePickerControllerDelegate ,UINavigat
         currentUser.printUser()
         
         
-        let image = UIImageJPEGRepresentation(photo.image!, 0.1) //UIImagePNGRepresentation(photo.image!)
+       //UIImagePNGRepresentation(photo.image!)
         
-        
-        Molocate.uploadProfilePhoto(image!) { (data, response, error) -> () in
+        var image = editProfile.RBSquareImageTo(photo.image!, size: CGSize(width: 192, height: 192))
+        let imageData = UIImageJPEGRepresentation(image, 0.5)
+        Molocate.uploadProfilePhoto(imageData!) { (data, response, error) -> () in
             print(data)
             currentUser.profilePic = NSURL(string: data!)!
         }
@@ -431,6 +432,66 @@ class editProfile: UIViewController , UIImagePickerControllerDelegate ,UINavigat
         line12.layer.borderColor = UIColor.grayColor().CGColor
         self.view.addSubview(line12)
         
+    }
+    
+    
+     class func RBSquareImageTo(image: UIImage, size: CGSize) -> UIImage {
+        return RBResizeImage(RBSquareImage(image), targetSize: size)
+    }
+    
+    class func RBSquareImage(image: UIImage) -> UIImage {
+        var originalWidth  = image.size.width
+        var originalHeight = image.size.height
+        var x: CGFloat = 0.0
+        var y: CGFloat = 0.0
+        var edge: CGFloat = 0.0
+        
+        if (originalWidth > originalHeight) {
+            // landscape
+            edge = originalHeight
+            x = (originalWidth - edge) / 2.0
+            y = 0.0
+            
+        } else if (originalHeight > originalWidth) {
+            // portrait
+            edge = originalWidth
+            x = 0.0
+            y = (originalHeight - originalWidth) / 2.0
+        } else {
+            // square
+            edge = originalWidth
+        }
+        
+        var cropSquare = CGRectMake(x, y, edge, edge)
+        var imageRef = CGImageCreateWithImageInRect(image.CGImage, cropSquare);
+        
+        return UIImage(CGImage: imageRef!, scale: UIScreen.mainScreen().scale, orientation: image.imageOrientation)
+    }
+    
+    class func RBResizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
+        let size = image.size
+        
+        let widthRatio  = targetSize.width  / image.size.width
+        let heightRatio = targetSize.height / image.size.height
+        
+        // Figure out what our orientation is, and use that to form the rectangle
+        var newSize: CGSize
+        if(widthRatio > heightRatio) {
+            newSize = CGSizeMake(size.width * heightRatio, size.height * heightRatio)
+        } else {
+            newSize = CGSizeMake(size.width * widthRatio,  size.height * widthRatio)
+        }
+        
+        // This is the rect that we've calculated out and this is what is actually used below
+        let rect = CGRectMake(0, 0, newSize.width, newSize.height)
+        
+        // Actually do the resizing to the rect using the ImageContext stuff
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        image.drawInRect(rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage
     }
     func dismissKeyboard() {
         //Causes the view (or one of its embedded text fields) to resign the first responder status.
